2f9a0c346b6e877411942869fd274c78a0fa9a80
ahmed khalil <ahmedsaberacu77@egmail.com>
2025-12-01 05:41:13 +0200

    print node security group id

--- eks/output.tf
+++ eks/output.tf
@@ -3,10 +3,11 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+output "worker_node_group_one_sg_id" {
+  value = module.eks.node_security_group_id
 }
+
+
 
 output "region" {
   description = "AWS region"
